9553fc4a7632194538de64175a40c32d64564236
zivtir <>
2021-03-28 21:53:43 +0300

    align to bitbucket

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ var cors = require('cors')
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 const got = require('got');
-const { response } = require('express');
 const port = 8000;
-let visited = new Map();
+let visited = new Map(); // holds all the visited urls
 let maxDepth;
 let maxPages;
 const app = express();
@@ -16,9 +15,9 @@ app.listen(port, () => {
 });
 
 
-startTime = Date.now ();
+startTime = new Date().getTime();
 function executingAt() {
-    return (Date.now() - startTime) / 1000;
+    return (new Date().getTime() - startTime) / 1000;
   }
 
 app.get('/scrape', async (req, res) => { 
@@ -27,7 +26,7 @@ app.get('/scrape', async (req, res) => {
     let scrapeurl = req.query.scrapeurl;
     maxDepth = req.query.depth;
     maxPages = req.query.maxpages;
-    await extractLinks(scrapeurl);
+    await extractLinks(scrapeurl); // return to the client only when all the results came
     console.log('done crawling... on server')
     let jsonObject = {};    
     for (let [key, value] of visited) {
@@ -36,68 +35,84 @@ app.get('/scrape', async (req, res) => {
     res.send(JSON.stringify(jsonObject))
 });
 
+// async method calling url return promise.
 async function callUrl(url){
-  console.log('before call ' + url)
+  console.log(executingAt() + ' before call ' + url)
   try {
     retUrl = await got(url);
   } catch (error) {
     return undefined;
   }
-  console.log('after call ' + url)
+  console.log(executingAt() + ' after call ' + url)
   return retUrl;
 }
 
 const extractLinks = async (url) => {
   try {
-      const queue = [];
+      const queue = []; // using queue for BFS
       var countPage = 1;
       var nextDepth;
-      visited.set(url,{depth:0,page:countPage,title:'temptitle'})
+      
+      visited.set(url,{title:'pagetitle',depth:0,page:countPage,pageLinks:[]})
       queue.push({url:url,depth:0});
       while (queue.length > 0 ) {
         let UrlPromises = [];
-        nextDepth = cururl.depth + 1;
-        do {
-          var cururl = queue.shift();
-          let promise = callUrl(cururl.url);
-          UrlPromises.push(promise)
-        } while (queue.length > 0);
+        nextDepth = iterateCallUrls(queue,UrlPromises);
 
         let urlResponses = await Promise.all(UrlPromises);
+        console.log(executingAt() + ' All promises were returned of depth '.concat(nextDepth -1))
         urlResponses.forEach(urlResponse => {
           if (urlResponse !== undefined) {
             const $ = cheerio.load(urlResponse.body);
             var title = $('title').text();
             try {
-              if (visited.get(urlResponse.url) !== undefined) {
-                visited.get(urlResponse.url).title=title;
-              }
-              else{
-                visited.get(urlResponse.url.slice(0, -1)).title=title;
-              }
+              getVisited(urlResponse.url).title=title;
             } catch (error) {}
             
-            if(countPage <= maxPages && nextDepth <= maxDepth){
-              const linkObjects = $('a');
-              linkObjects.each((index, element) => {
-                var href = $(element).attr('href');
-                if (typeof href === 'string' && href.includes('http')){
-                  if (!visited.has(href)) {
-                    if(++countPage > maxPages)
-                      return false;
-                    queue.push({url:href,depth:nextDepth}); 
-                    visited.set(href,{depth:nextDepth,page:countPage,title:'temptitle'})
-                  }
+            const linkObjects = $('a');
+            var links =[];
+            linkObjects.each((index, element) => {
+              var href = $(element).attr('href');
+              if (typeof href === 'string' && href.includes('http')){
+                links.push(href); // for result presentation
+                if(countPage + 1 <= maxPages && nextDepth <= maxDepth && !visited.has(href)){
+                  countPage++;
+                  queue.push({url:href,depth:nextDepth}); 
+                  visited.set(href,{title:'pagetitle',depth:nextDepth,page:countPage,pageLinks:[]})
                 }
-              });
-            }
+              }
+            });
+            try {
+              getVisited(urlResponse.url).pageLinks=links;
+            } catch (error) {}
           }
         });
+        console.log(executingAt() + ' ===========> done break depth ' +  nextDepth)
     };
   } catch (error) {
-    console.log('*******000*****\n' + error);
+    console.log('************\t' + error);
   }
 };
+
+// fetch each child url of a concreate page
+function iterateCallUrls(queue,UrlPromises){
+  do {
+    var cururl = queue.shift();
+    nextDepth = cururl.depth + 1;
+    let promise = callUrl(cururl.url);
+    UrlPromises.push(promise)
+  } while (queue.length > 0);
+  return nextDepth;
+}
+ 
+// return a visited url
+function getVisited(url){
+  if (visited.get(url) !== undefined) {
+    return visited.get(url);
+  }
+  else
+    return visited.get(url.slice(0, -1));
+}
 
 
 
